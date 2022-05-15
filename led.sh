badd8aa824608cb9757b25a53d456e8e9afaeadd
skyrid3r698 <79767107+skyrid3r698@users.noreply.github.com>
2022-05-15 16:55:02 +0200

    Update led.sh

--- led.sh
+++ led.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-   sleep 0.3 &
+   sleep 0.6 &
    while IFS=" " read -r col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11 col12 col13 col14 col15; do
      TEST="$col10"
    done < "/sys/block/sda/stat"
@@ -9,9 +9,11 @@ while true; do
   then
     echo greater
     echo 0 > /sys/class/gpio/gpio4/value
-    sleep 0.06
+    sleep 0.2
     echo 1 > /sys/class/gpio/gpio4/value
-  else
+    sleep 0.2
+  elif [ $TEST -eq $TEST2 ]
+  then
     echo same
     echo 0 > /sys/class/gpio/gpio4/value
   fi
