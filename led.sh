d549abf5c22f345d8c5987e31c26d299b594ccef
skyrid3r698 <79767107+skyrid3r698@users.noreply.github.com>
2022-05-15 16:43:50 +0200

    Update led.sh

--- led.sh
+++ led.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-   sleep 0.2 &
+   sleep 0.3 &
    while IFS=" " read -r col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11 col12 col13 col14 col15; do
      TEST="$col10"
    done < "/sys/block/sda/stat"
